ae23e78e096d0e2ee7b9630fda52eaf4ff44fdf2
Hasper51 <ainur.vahitov2002@gmail.com>
2022-04-06 17:56:42 +0300

    создание нормальной клавиатуры

---
+++ keyboard-buttons.js
@@ -0,0 +1,8 @@
+module.exports = {
+  home: {
+    switch_on: 'Включить',
+    switch_off: 'Отключить',
+    status: 'Текущий статус'
+  }
+
+}
---
+++ keyboard.js
@@ -0,0 +1,8 @@
+const kb = require('./keyboard-buttons')
+
+module.exports = {
+  home: [
+    [kb.home.switch_on, kb.home.switch_off],
+    [kb.home.status]
+  ]
+}
