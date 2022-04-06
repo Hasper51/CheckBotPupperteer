dcc672c1a835bc682fd3dbcedb90864492f6ab73
Hasper51 <ainur.vahitov2002@gmail.com>
2022-04-06 19:29:57 +0300

    release

--- keyboard.js
+++ keyboard.js
@@ -3,6 +3,11 @@ const kb = require('./keyboard-buttons')
 module.exports = {
   home: [
     [kb.home.switch_on, kb.home.switch_off],
-    [kb.home.status]
+    [kb.home.status],
+    //[kb.home.manage],
+    [kb.home.updates]
+  ],
+  manage: [
+    [kb.back]
   ]
 }
