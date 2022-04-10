8003d95596676523a0ff17d3c9d919e8bf755153
Hasper51 <ainur.vahitov2002@gmail.com>
2022-04-10 13:23:21 +0300

    alfa 1.2.2

--- keyboard-buttons.js
+++ keyboard-buttons.js
@@ -1,20 +1,18 @@
+let add_points={
+  manage: 'Управление',
+  updates: 'Обновления'
+}
 module.exports = {
+   
   home_1: {
     switch: 'Включить ✅',
-    // switch_on: 'Включить',
-    // switch_off: 'Отключить',
-    status: 'Текущий статус',
-    manage: 'Управление',
-    updates: 'Обновления'
+    ...add_points
   }, 
   home_2: {
     switch: 'Отключить ❌',
-    // switch_on: 'Включить',
-    // switch_off: 'Отключить',
-    status: 'Текущий статус',
-    manage: 'Управление',
-    updates: 'Обновления'
-  }, 
+    ...add_points
+  },
+  
   manage: {
     
   },
--- keyboard.js
+++ keyboard.js
@@ -3,16 +3,10 @@ const kb = require('./keyboard-buttons')
 module.exports = {
   home_1: [
     [kb.home_1.switch],
-    // [kb.home_1.switch_on, kb.home.switch_off],
-    // [kb.home_1.status],
-    //[kb.home_1.manage],
     [kb.home_1.updates]
   ],
   home_2: [
     [kb.home_2.switch],
-    // [kb.home_2.switch_on, kb.home.switch_off],
-    // [kb.home_2.status],
-    //[kb.home_2.manage],
     [kb.home_2.updates]
   ],
   manage: [
