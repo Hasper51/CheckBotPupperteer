8aa048755be9158936840ad4c79e0e2c20d923d6
Hasper51 <ainur.vahitov2002@gmail.com>
2022-04-12 01:40:26 +0300

    инлайн клава и полу рабочая система дисциплин

--- keyboard.js
+++ keyboard.js
@@ -3,13 +3,17 @@ const kb = require('./keyboard-buttons')
 module.exports = {
   home_1: [
     [kb.home_1.switch],
-    [kb.home_1.updates]
+    [kb.home_1.updates],
+    [kb.home_1.manage]
   ],
   home_2: [
     [kb.home_2.switch],
-    [kb.home_2.updates]
+    [kb.home_2.updates],
+    [kb.home_1.manage]
   ],
   manage: [
+    [kb.manage.settings],
     [kb.back]
   ]
+  
 }
