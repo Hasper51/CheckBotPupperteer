7357134b0a35b535a603b793d93803b216e31c48
Hasper51 <ainur.vahitov2002@gmail.com>
2022-04-11 12:18:51 +0300

    alpha 1.2.3

--- test.js
+++ test.js
@@ -1,21 +1,29 @@
-const puppeteer = require('puppeteer');
-const data = require('./data.json');
-main();
+var max = 1;
 
-async function main(){
-  const browser = await puppeteer.launch({headless:false})
-  const page = await browser.newPage();
-  await page.goto('https://google.com')
+var p = Promise.reject();
+for(var i=0; i<max; i++) {
+	p = p.catch(attempt).then(test);
+}
+p = p.then(processResult).catch(errorHandler);
 
-  
-  let login = data[i].login;
-  let password = data[i].password;
-  await page.waitForSelector('#users')
-  await page.type('#users', login)
-  await page.type('#parole', password)
-  await page.click('#logButton')
-  await page.waitForSelector('#heading1')
-  await page.click('#heading1')
-
-  await browser.close();
+function attempt() {
+	var rand = Math.random();
+	if(rand < 0.8) {
+		throw rand;
+	} else {
+		return rand;
+	}
+}
+function test(val) {
+	if(val < 0.9) {
+		throw val;
+	} else {
+		return val;
+	}
+}
+function processResult(res) {
+	console.log(res);
+}
+function errorHandler(err) {
+	console.error(err);
 }
