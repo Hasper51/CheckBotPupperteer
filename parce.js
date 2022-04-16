505aec156b2b1d180b4e4a3ee73ae2caf3b39176
Hasper51 <ainur.vahitov2002@gmail.com>
2022-04-16 13:04:01 +0300

    alpha 1.3

---
+++ parce.js
@@ -0,0 +1,72 @@
+const puppeteer = require('puppeteer');
+const data = require('./data.json');
+const fs = require('fs')
+module.exports = schedule;
+async function schedule(url, groupNumber) {
+    let startTime = Date.now();
+    const browser = await puppeteer.launch({ headless: true , defaultViewport: null});
+    const page = await browser.newPage();
+    //await page.setUserAgent(userAgent.toString());
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    
+    //await page.evaluate(val => document.querySelector('input[name="field-keywords"]').value = val, searchTerm);
+
+    let link = await page.$(`a[data-nm='${groupNumber}']`);
+
+    //console.log("link: "+link)
+    await link.click();
+    //await page.waitForSelector('#rasp-prev')
+    await page.waitForNavigation({waitUntil: 'networkidle2'});
+    
+    
+    const result = await page.evaluate(() => {
+        let data_mas = [];
+        
+        let subjects_length = document.querySelectorAll('.vt244').length;
+        
+        for (let i = 1; i <= 6; i++) {
+            
+            data_obj = {}
+            for (let j = 0; j < subjects_length-1; j++) {
+                let count = document.querySelectorAll(".vt283")[j].textContent
+                let obj = document.querySelectorAll(`.rasp-day${i}`)[j]
+                //let status = obj.querySelector('.vt240')==null  ? false : true
+                if(obj.querySelector('.vt240')!==null){
+                    let type = obj.querySelector('.vt243').textContent.replace(/\t|\n|[0-9]|[()]/g, '')
+                    if(type=='Лабораторная работа')type = 'Практические занятия'
+                    let title = obj.querySelector('.vt240').textContent.replace(/\t|\n|[0-9]|[()]/g, '').trimEnd()
+                    data_obj[`${count}`] = {
+                        title: title,
+                        type: type,
+                        status: true
+                    }
+                }else data_obj[`${count}`] = null
+                //data_obj.type = obj.querySelector('.vt240')==null  ? '' : obj.querySelector('.vt240').textContent.replace(/\t|\n|[0-9]|[()]/g, '').trimEnd()
+                
+                //data_obj.type = obj.querySelector('.vt243').innerText;
+                
+                
+                
+            }
+            data_mas.push(data_obj)
+            
+        }
+        return data_mas
+    });
+    console.log(result)
+    data.active.forEach(elem => {
+        if (elem.group==groupNumber){
+            elem.disciplines=result
+        }
+    })
+    fs.writeFileSync("data.json", JSON.stringify(data))
+    
+    
+    
+    await browser.close();
+
+    console.log('Time: ', (Date.now() - startTime) / 1000, 's');
+}
+
+//schedule('https://www.sut.ru/studentu/raspisanie/raspisanie-zanyatiy-studentov-ochnoy-i-vecherney-form-obucheniya', 'ИКТ-113');
